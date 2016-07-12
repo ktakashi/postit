1f41303b370f021b9dec58270d213ad611899df4
Takashi Kato <ktakashi@ymail.com>
2016-07-12 16:29:00 +0200

    Fixing sequence name for PostgreSQL

--- sql/alter_table_pg.sql
+++ sql/alter_table_pg.sql
@@ -1,4 +1,4 @@
 -- PostgreSQL specific
-alter table users alter column id set default nextval('user_seq');
+alter table users alter column id set default nextval('users_seq');
 alter table user_info alter column id set default nextval('user_info_seq');
 alter table postit alter column id set default nextval('postit_seq');
--- sql/create_sequence.sql
+++ sql/create_sequence.sql
@@ -1,3 +1,3 @@
-create sequence user_seq start with 100;
+create sequence users_seq start with 100;
 create sequence user_info_seq;
 create sequence postit_seq;
